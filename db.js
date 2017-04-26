61e54bbe2a1c2e57fd4dab2ec99f2793e59530dd
Amber Tsosie <atxambra@gmail.com>
2017-04-26 01:17:58 +0000

    Created db.js to separate data access and input/output. Refactored and cleaned up code.

---
+++ db.js
@@ -0,0 +1,24 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const config = {
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+};
+
+module.exports = {
+
+  connect: (done) => {
+    const client = new pg.Client(config);
+    client.connect((error) => {
+      if (error) {
+        return console.error("Connection Error", error);
+      }
+      done(error, client);
+    });
+  }
+};
--- lookup_people.js
+++ lookup_people.js
@@ -1,53 +1,42 @@
-const pg = require("pg");
-const settings = require("./settings"); // settings.json
+const db = require("./db");
 
-const client = new pg.Client({
-  user     : settings.user,
-  password : settings.password,
-  database : settings.database,
-  host     : settings.hostname,
-  port     : settings.port,
-  ssl      : settings.ssl
-});
+const lookupName = process.argv[2];
 
-client.connect((err) => {
-  if (err) {
-    return console.error("Connection Error", err);
+function findPersonByName(lookupName, done) {
+  db.connect((err, client) => {
+    client.query("SELECT id, first_name, last_name, to_char(birthdate, 'YYYY-MM-DD') birthdate FROM famous_people WHERE first_name = $1::text OR last_name = $1::text", [lookupName], (err, queryResult) => {
+      done(err, queryResult);
+      client.end();
+    });
+  });
+}
+
+function logOutputHeader(queryResult) {
+  console.log("Searching ...");
+  console.log(`Found ${queryResult.rows.length} person(s) by the name '${lookupName}'`);
+}
+
+function logPersonInfo(queryResult) {
+  for (idx in queryResult.rows) {
+    let person = {
+      id: queryResult.rows[idx].id,
+      first_name: queryResult.rows[idx].first_name,
+      last_name: queryResult.rows[idx].last_name,
+      birthdate: queryResult.rows[idx].birthdate
+    };
+    console.log(`- ${person.id}: ${person.first_name} ${person.last_name}, born '${person.birthdate}'`);
   }
+}
 
+
+findPersonByName(lookupName, (err, queryResult) => {
   if (process.argv.length !== 3) {
     console.log("Usage: node lookup_people.js <name>");
     process.exit(1);
   }
-
-  const lookupName = process.argv[2];
-
-  client.query("SELECT id, first_name, last_name, to_char(birthdate, 'YYYY-MM-DD') birthdate FROM famous_people WHERE first_name = $1::text OR last_name = $1::text", [lookupName], (err, queryResult) => {
-
-    if (err) {
-      return console.error("error running query", err);
-    }
-
-    function logOutputHeader(queryResult) {
-      console.log("Searching ...");
-      console.log(`Found ${queryResult.rows.length} person(s) by the name '${lookupName}'`);
-    }
-
-    function logPersonInfo(queryResult) {
-      for (idx in queryResult.rows) {
-        let person = {
-          id: queryResult.rows[idx].id,
-          first_name: queryResult.rows[idx].first_name,
-          last_name: queryResult.rows[idx].last_name,
-          birthdate: queryResult.rows[idx].birthdate
-        };
-        console.log(`- ${person.id}: ${person.first_name} ${person.last_name}, born '${person.birthdate}'`);
-      }
-    }
-
-    logOutputHeader(queryResult);
-    logPersonInfo(queryResult);
-
-    client.end();
-  });
+  if (err) {
+    return console.error("error running query", err);
+  }
+  logOutputHeader(queryResult);
+  logPersonInfo(queryResult);
 });
